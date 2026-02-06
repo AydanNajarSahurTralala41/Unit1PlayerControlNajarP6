7155d213b8442799ac769badc7c2537b0d183ded
AydanNajarSahurTralala41 <726038@student.dusd.net>
2026-02-06 13:52:07 -0800

    Finished scripts
    finished my scripts now working on the bonus

--- P6PlayerControlNajar/Assets/Scripts/FollowPlayer.cs
+++ P6PlayerControlNajar/Assets/Scripts/FollowPlayer.cs
@@ -14,6 +14,6 @@ public class FollowPlayer : MonoBehaviour
     // Update is called once per frame
     void LateUpdate()
     {
-        transform.position = player.transform.position + new Vector3(0, 5, -7);
+        transform.position = player.transform.position + offset;
     }
 }
--- P6PlayerControlNajar/Assets/Scripts/PlayerController.cs
+++ P6PlayerControlNajar/Assets/Scripts/PlayerController.cs
@@ -5,15 +5,24 @@ public class PlayerController : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
 
-    public float speed = 5.0f;
+    private float speed = 20.0f;
+    private float turnSpeed = 45.0f;
+    private float horizontalInput;
+    private float forwardInput;
 
     void Update()
-    {
-        transform.Translate(Vector3.forward * Time.deltaTime * 20);
+    { horizontalInput = Input.GetAxis("Horizontal");
+        forwardInput = Input.GetAxis("Vertical");
+        // Moves the car forward based on a vertical input
+        transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
+        // Rotates the car based on horizontal input
+        transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+
     }
-}
+}        
